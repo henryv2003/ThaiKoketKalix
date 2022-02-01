60e5d1831d050ed88d603cae9126021df21a6327
henryv2003 <alexandervh1986@gmail.com>
2022-02-02 00:08:46 +0100

    new edit

--- js/script.js
+++ js/script.js
@@ -52,3 +52,32 @@ menuTabs.addEventListener("click", function(e){
        AOS.init();
    }
 }) 
+
+/*---------------------- buffe menu ------------------------------*/
+
+/*const buffeMenuTabs = document.querySelector(".menu-tabs");
+menuTabs.addEventListener("click", function(e){
+   if(e.target.classList.contains("menu-tab-item") && !e.target.classList.contains("active")){
+       const target = e.target.getAttribute("data-target");
+       menuTabs.querySelector(".active").classList.remove("active");
+       e.target.classList.add("active");
+       const menuSection = document.querySelector(".menu-section");
+       menuSection.querySelector(".menu-tab-content.active").classList.remove("active");
+       menuSection.querySelector(target).classList.add("active");
+
+     
+     
+     
+       /*<button type="button" class="menu-tab-item" data-target="#licor">standard</button>
+      
+       <div class="row menu-tab-content active" id="appetizer">
+       <div class="menu-item" data-aos="fade-up-right">
+         <div class="menu-item-title">
+           <img src="img/logga.png" alt="">
+           <h3>1. vårrule <br><span>a)Kicking b)Köttfars c)Vegetarisk d)Lax</span></h3>
+           
+         </div>
+           <div class="menu-item-price">
+             59.00
+         </div>
+       </div>-----*/
